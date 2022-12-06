710a5c590a80137f8883a34a85a59602a324cda5
GVRemington <104083156+GVRemington@users.noreply.github.com>
2022-12-06 13:08:15 -0800

    Add project files.

---
+++ StringMeAlong_Static_Roberts/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Roberts, Rayleen
+//IT 113
+//Notes: This is a redo
+//Behaviors Not Implemented and Why: String Reverse keeping case...can't figure it out. 
+//                                   Also, this is the static version so ToString and Equals
+//                                   are just static methods, no override
+
+using StringMeAlong_Static_Roberts;
+
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(StringManager.goBackwards("Sunbeam Tiger"));
+Console.WriteLine(StringManager.ToString("Sunbeam Tiger"));
+Console.WriteLine("\t\nIs ABBA Symetrical?  " + StringManager.symetrical("ABBA"));
+Console.WriteLine("\t\nIs ABA  Symetrical?  " + StringManager.symetrical("ABA"));
+Console.WriteLine("\t\nIs ABba Symetrical?  " + StringManager.symetrical("ABba"));
+Console.WriteLine("\t\n Does Sunbeam Tiger = Sunbeam Tiger? " + (StringManager.Equals("Sunbeam Tiger", "Sunbeam Tiger")));
+Console.WriteLine("\t\n Does Tiger Sunbeam = Sunbeam Tiger? " + (StringManager.Equals("Sunbeam Tiger", "Tiger Sunbeam")));
+
+//Console.WriteLine(StringManager.casePreservation("Lets Give This A Try"));//not working correctly I remarked it out
---
+++ StringMeAlong_Static_Roberts/StringManager.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringMeAlong_Static_Roberts
+{
+    static class StringManager
+    {
+
+        public static string _getData(string manipulateThis)
+        { return manipulateThis; }
+
+        public static string goBackwards(string toReverse)
+        {
+            string reverse = string.Empty;
+            Char[] array = toReverse.ToCharArray();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reverse = reverse + array[i];
+            }
+            return "\n\t The reverse of Sunbeam Tiger is:  " + reverse;
+        }
+
+        public static bool symetrical(string A)
+        {
+            bool _symetrical = false;
+            string B = string.Empty;
+            char[] array = A.ToCharArray();
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                B = B + array[i];
+            }
+            foreach (char c in A)
+            {
+                foreach (char c2 in B)
+                {
+                    if (c == c2)
+                    { _symetrical = true; }
+                    else
+                    { _symetrical = false; }
+                }
+            }
+            return _symetrical;
+
+        }
+        public static bool Equals(object Bob, object Fred)
+        {
+            string A = _getData((string)Bob);
+            string B = _getData((string)Fred);
+            return A.Equals(B);
+        }
+
+        public static string ToString(string toString)
+        {
+            double value = 0;
+            byte[] array = Encoding.ASCII.GetBytes(toString);
+            string num = string.Empty;
+
+
+            foreach (byte b in array)
+            {
+                value = value + b;
+
+            }
+
+            string valueS = value.ToString();
+
+            foreach (char c in valueS) // lots of copy and paste here
+            {
+
+                if (c == '0')
+                {
+                    num = num + "zero ";
+                }
+                if (c == '1')
+                {
+                    num = num + "one ";
+                }
+                if (c == '2')
+                {
+                    num = num + "two ";
+                }
+                if (c == '3')
+                {
+                    num = num + "three ";
+                }
+                if (c == '4')
+                {
+                    num = num + "four ";
+                }
+                if (c == '5')
+                {
+                    num = num + "five ";
+                }
+                if (c == '6')
+                {
+                    num = num + "six ";
+                }
+                if (c == '7')
+                {
+                    num = num + "seven";
+                }
+                if (c == '8')
+                {
+                    num = num + "eight ";
+                }
+                if (c == '9')
+                {
+                    num = num + "nine ";
+                }
+            }
+            return "\n\tThe values of Sunbeam Tiger are: " + value + " and: " + num;
+        }
+
+        //public string casePreservation(string preserveCapitals)//not working
+        //{
+        //    char[] array = preserveCapitals.ToCharArray();
+        //    bool upper = true;
+        //    string reverse = string.Empty;
+        //    char[] array2 = reverse.ToCharArray();
+        //    string finalReverse = string.Empty;
+
+        //    foreach (char c in array)
+        //    {
+        //        for (int i = array.Length - 1; i >= 0; i--)
+        //        {
+        //            if (char.IsUpper(c))
+        //            {
+        //                reverse = reverse + char.ToUpper(c);
+        //            }
+        //            else
+        //            {
+        //                reverse = reverse + char.ToLower(c);
+        //            }
+
+        //        }
+        //        //Console.WriteLine(c);
+        //    }
+
+        //    return reverse;
+
+
+
+    }
+}
